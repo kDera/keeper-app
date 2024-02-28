6bd65b4d65e9b153de14eb70c7b7ff9fd747175a
kDera <caduvazquezabj@gmail.com>
2024-02-28 09:45:58 -0300

    28/02

---
+++ src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+    <div className="note">
+        <h1> Título da nota </h1>
+        <p> infos da nota </p>
+    </div>
+    );
+}
+
+export default Note;
